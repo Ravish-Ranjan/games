24700a559678b39ef1a5d806d7c8436d030c6b91
Ravish Ranjan <122588009+Ravish-Ranjan@users.noreply.github.com>
2024-03-23 14:09:26 +0530

    nav bar design

--- client/src/scripts/Nav.js
+++ client/src/scripts/Nav.js
@@ -1,12 +1,46 @@
-import logo from "../media/logo back.png";
-import "../styles/Nav.css";
+import logo from "../assets/logo large.png";
+import "../styles/Nav.scss";
+import { useState } from "react";
+import IconButton from "@mui/material/IconButton";
+import SearchIcon from "@mui/icons-material/Search";
+import TextField from "@mui/material/TextField";
+import Avatar from '@mui/material/Avatar';
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { deepPurple } from '@mui/material/colors';
+
+const darkTheme = createTheme({
+	palette: {
+		mode: "dark",
+	},
+});
 
 export function Nav() {
+	const [SearchQuery, setSearchQuery] = useState("");
 	return (
 		<nav>
-			<div>
+			<div id="brand">
 				<img src={logo} alt=""></img>
-				<span>Games</span>
+				<span>SOCK8's</span>
+			</div>
+			<div id="nav-control">
+				<ThemeProvider theme={darkTheme}>
+					<TextField
+						id="search-bar"
+						className="text"
+						onInput={(e) => {
+							setSearchQuery(e.target.value);
+						}}
+						label="Search games"
+						variant="outlined"
+						color="secondary"
+						size="small"
+						value={SearchQuery}
+					/>
+					<IconButton type="submit" aria-label="search">
+						<SearchIcon style={{ fill: "hsl(300, 40%, 8	0%)" }} />
+					</IconButton>
+				</ThemeProvider>
+				<Avatar sx={{ bgcolor: deepPurple[400] }} alt="Ravish Ranjan" className="avatar pixel-font">Rr</Avatar>
 			</div>
 		</nav>
 	);
