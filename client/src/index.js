150639615aa8173ceea9aa1328a02e47a7905f25
Ravish Ranjan <122588009+Ravish-Ranjan@users.noreply.github.com>
2024-05-02 20:44:07 +0530

    logo redesign

--- client/src/index.js
+++ client/src/index.js
@@ -2,14 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./styles/index.scss";
 import { Nav } from "./scripts/Nav";
-import { Slider } from "./scripts/Slider";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
 	<React.StrictMode>
 		<Nav />
-		<Slider />
 	</React.StrictMode>
 );
 
